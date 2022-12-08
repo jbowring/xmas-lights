847008d1c81cc45f81ced818825f7e1e6c436a6a
jbowring <joelbowring@me.com>
2022-12-08 17:27:13 +0000

    Move back to synchronous webserver

--- xmas-lights-upython/main.py
+++ xmas-lights-upython/main.py
@@ -8,10 +8,11 @@ import html
 import secrets
 import neopixel
 import machine
-import uasyncio
+import usocket as socket
 import sys
 import io
 import _thread
+import gc
 
 max_leds = 40
 # led_strip = neopixel.NeoPixel(max_leds, 0, 22, 'GRB')  # TODO
@@ -43,10 +44,12 @@ def reconnect_wifi():
 
     if ip_address != new_ip_address:
         ip_address = new_ip_address
-        # TODO make async
-        request = urequests.get(secrets.ddns_url + f'&myip={ip_address}')
-        print(request.text)
-        request.close()
+        try:
+            request = urequests.get(secrets.ddns_url + f'&myip={ip_address}')
+            print(request.text)
+            request.close()
+        except Exception as exception:
+            print(repr(exception))
 
 
 reset = True
@@ -79,98 +82,6 @@ class HttpRequest:
             self.body = request_head_body[1]
         except IndexError:
             self.body = ''
-
-
-async def serve_client(reader, writer):
-    global reset
-    raw_request = b''
-    while recv := await reader.read(1024):
-        raw_request += recv
-
-        if len(recv) < 1024:
-            break
-
-    try:
-        content_length = int(HttpRequest(raw_request).headers['Content-Length'])
-        timeout_ms = 1000
-        end_ticks = time.ticks_add(time.ticks_ms(), timeout_ms)
-        while len(HttpRequest(raw_request).body) < content_length and time.ticks_diff(end_ticks, time.ticks_ms()) > 0:
-            raw_request += await uasyncio.wait_for_ms(reader.read(1024), timeout_ms)
-    except (IndexError, ValueError, KeyError, uasyncio.TimeoutError):
-        pass
-
-    request = HttpRequest(raw_request)
-    print("Request:", request.request_line)
-
-    if request.request_line.startswith('POST') or request.request_line.startswith('DELETE'):
-        try:
-            post_data = json.loads(request.body)
-        except (IndexError, ValueError):
-            writer.write('HTTP/1.0 400 Bad Request\r\nContent-type: text/plain\r\n\r\n')
-            await writer.drain()
-            await writer.wait_closed()
-            return
-
-        if request.request_line.startswith('DELETE'):
-            try:
-                del patterns[post_data['id']]
-            except KeyError:
-                writer.write('HTTP/1.0 400 Bad Request\r\nContent-type: text/plain\r\n\r\n')
-                await writer.drain()
-                await writer.wait_closed()
-                return
-            else:
-                with open('patterns.json', 'w') as file:
-                    file.write(json.dumps(patterns))
-        elif request.request_line.startswith('POST'):
-            json_keys = {
-                'active': bool,
-                'name': str,
-                'author': str,
-                'script': str,
-            }
-
-            def key_valid(post_data, key, key_type):
-                return key in post_data and isinstance(post_data[key], key_type)
-
-            if key_valid(post_data, 'id', str):
-                pattern_id = post_data['id']
-            else:
-                pattern_id = str(random.getrandbits(32))
-
-            # allow partial update if pattern_id already exists
-            if pattern_id in patterns or all(key_valid(post_data, key, key_type) for key, key_type in json_keys.items()):
-                if pattern_id not in patterns:
-                    patterns[pattern_id] = {}
-                do_reset = patterns[pattern_id].get('active', False) or post_data.get('active', False)
-                for key, key_type in json_keys.items():
-                    if key_valid(post_data, key, key_type):
-                        if key == 'active' and post_data[key] is True:
-                            patterns[pattern_id]['error'] = None
-                            for pattern in patterns.values():
-                                pattern['active'] = False
-                        patterns[pattern_id][key] = post_data[key]
-
-                if do_reset:
-                    reset = True
-
-                with open('patterns.json', 'w') as file:
-                    file.write(json.dumps(patterns))
-
-        writer.write('HTTP/1.0 200 OK\r\nContent-type: text/plain\r\n\r\n')
-        await writer.drain()
-        await writer.wait_closed()
-    else:
-        writer.write('HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n')
-        writer.write(html.generate_html(json.dumps(json.dumps(patterns))))
-        await writer.drain()
-        await writer.wait_closed()
-
-
-async def main():
-    print('Setting up webserver...')
-    await uasyncio.get_event_loop().create_task(uasyncio.start_server(serve_client, "0.0.0.0", 80))
-    uasyncio.get_event_loop().run_forever()
 
 
 def led_thread():
@@ -300,7 +211,83 @@ def led_thread():
 
 _thread.start_new_thread(led_thread, ())
 
-try:
-    uasyncio.run(main())
-finally:
-    uasyncio.new_event_loop()
+addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
+
+s = socket.socket()
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.bind(addr)
+s.listen(1)
+
+print('listening on', addr)
+
+while True:
+    cl, addr = s.accept()
+
+    request = HttpRequest(cl.recv(1024))
+    print("Request:", request.request_line)
+
+    if request.request_line.startswith('POST') or request.request_line.startswith('DELETE'):
+        try:
+            post_data = json.loads(request.body)
+        except (IndexError, ValueError):
+            cl.send('HTTP/1.0 400 Bad Request\r\nContent-type: text/plain\r\n\r\n')
+            cl.close()
+            continue
+
+        if request.request_line.startswith('DELETE'):
+            try:
+                del patterns[post_data['id']]
+            except KeyError:
+                cl.send('HTTP/1.0 400 Bad Request\r\nContent-type: text/plain\r\n\r\n')
+                cl.close()
+                continue
+            else:
+                with open('patterns.json', 'w') as file:
+                    file.write(json.dumps(patterns))
+        elif request.request_line.startswith('POST'):
+            json_keys = {
+                'active': bool,
+                'name': str,
+                'author': str,
+                'script': str,
+            }
+
+            def key_valid(post_data, key, key_type):
+                return key in post_data and isinstance(post_data[key], key_type)
+
+            if key_valid(post_data, 'id', str):
+                pattern_id = post_data['id']
+            else:
+                pattern_id = str(random.getrandbits(32))
+
+            # allow partial update if pattern_id already exists
+            if pattern_id in patterns or all(key_valid(post_data, key, key_type) for key, key_type in json_keys.items()):
+                if pattern_id not in patterns:
+                    patterns[pattern_id] = {}
+                do_reset = patterns[pattern_id].get('active', False) or post_data.get('active', False)
+                for key, key_type in json_keys.items():
+                    if key_valid(post_data, key, key_type):
+                        if key == 'active' and post_data[key] is True:
+                            patterns[pattern_id]['error'] = None
+                            for pattern in patterns.values():
+                                pattern['active'] = False
+                        patterns[pattern_id][key] = post_data[key]
+
+                if do_reset:
+                    reset = True
+
+                with open('patterns.json', 'w') as file:
+                    file.write(json.dumps(patterns))
+
+        cl.send('HTTP/1.0 200 OK\r\nContent-type: text/plain\r\n\r\n')
+        cl.close()
+    else:
+        cl.send('HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n')
+        to_send = html.generate_html(json.dumps(json.dumps(patterns)))
+        chunk = 4096
+        for i in range(int(len(to_send)/chunk)):
+            cl.sendall(to_send[i*chunk:(i+1)*chunk])
+            print(gc.mem_free())
+        cl.sendall(to_send[(i+1)*chunk:-1])
+        cl.close()
+        print(gc.mem_free())
