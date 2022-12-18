e765d637c6c527aaf5b3cf381a1fea8f7035ce8f
jbowring <joelbowring@me.com>
2022-12-18 23:29:26 +0000

    Add service and install script

---
+++ xmas-lights-flask/install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+name="xmaslights"
+dir=$(dirname "$0")
+
+apt update &&
+apt install python3-pip -y &&
+pip3 install -r "$dir/requirements.txt" &&
+cp "$dir/$name.service" /lib/systemd/system/ &&
+systemctl daemon-reload &&
+systemctl enable "$name" &&
+chmod +x "$dir/app.py" &&
+service "$name" start
