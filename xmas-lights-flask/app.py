e66f5b8d8fbb02fd2af6bb8f4718c4668220344e
jbowring <joelbowring@me.com>
2022-12-19 13:00:53 +0000

    Increase update rate

--- xmas-lights-flask/app.py
+++ xmas-lights-flask/app.py
@@ -17,7 +17,7 @@ max_leds = 40
 led_strip = rpi_ws281x.PixelStrip(max_leds, 18, strip_type=rpi_ws281x.WS2811_STRIP_GRB)
 led_strip.begin()
 
-timestep = 0.1
+timestep = 0.01
 
 # Global scope for script interpretation
 GLOBAL_SCOPE = {
