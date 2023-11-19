d4ed771b7d066fd02a235628f9fc22ffa1d2ae4a
jbowring <joel@bowring.uk>
2023-11-19 13:28:22 +0000

    Use install script for deployment

--- backend/install.sh
+++ backend/install.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
+set -e
+
 name="xmaslights"
 dir=$(dirname "$0")
 
-apt update &&
-apt install python3-pip -y &&
-pip3 install -r "$dir/requirements.txt" &&
-cp "$dir/$name.service" /lib/systemd/system/ &&
-systemctl daemon-reload &&
-systemctl enable "$name" &&
-chmod +x "$dir/app.py" &&
-service "$name" start
+apt update
+apt install python3-pip -y
+pip3 install -r "$dir/requirements.txt"
+cp "$dir/$name.service" /lib/systemd/system/
+systemctl daemon-reload
+systemctl enable "$name"
+chmod +x "$dir/app.py"
+service "$name" restart
